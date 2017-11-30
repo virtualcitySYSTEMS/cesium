926ab3fc9ac2e051a853ac2ea80518bf89129516
Ben Kuster <bkuster@virtualcitysystems.de>
2017-11-30 13:36:50 +0100

    camera enable terrain adjustment when loading option

--- Source/Scene/Camera.js
+++ Source/Scene/Camera.js
@@ -224,6 +224,14 @@ define([
          */
         this.maximumZoomFactor = 1.5;
 
+        /**
+         * Allows or disallows camera position adjustment while terrain is loading.
+         * When <code>false</code> the camera position will only be adjusted when all terrain has been loaded.
+         * @type {Boolean}
+         * @default false
+         */
+        this.enableTerrainAdjustmentWhenLoading = false;
+
         this._moveStart = new Event();
         this._moveEnd = new Event();
 
@@ -426,7 +434,7 @@ define([
         var minimumCollisionTerrainHeight = screenSpaceCameraController.minimumCollisionTerrainHeight;
         var minimumZoomDistance = screenSpaceCameraController.minimumZoomDistance;
 
-        if (this._suspendTerrainAdjustment || !enableCollisionDetection) {
+        if ((!this.enableTerrainAdjustmentWhenLoading && this._suspendTerrainAdjustment) || !enableCollisionDetection) {
             return;
         }
 
