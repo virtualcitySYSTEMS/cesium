2976cb16210840aeff86070963cb16efe1ebb305
jbo023 <jbolling@virtualcitysystems.de>
2019-04-17 15:07:04 +0200

    add cache.add guard to not allow adding the same key twicee

--- Source/Core/Cache.js
+++ Source/Core/Cache.js
@@ -25,6 +25,10 @@ define([], function() {
      * @param {Object} item The item to add.
      */
     Cache.prototype.add = function(key, item) {
+        if (this.has(key)){
+           this._map[key] = item;
+           return;
+        }
         if (this._index === this._maxSize) {
             this._full = true;
             this._index = 0;
