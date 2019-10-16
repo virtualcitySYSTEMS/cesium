3e683d025ddf2cf918c2f153ad98b9a4df548500
jbo023 <jbolling@virtualcitysystems.de>
2019-10-16 19:42:12 +0200

    rebatch directly, do not wait 120 fps

--- Source/Scene/Vector3DTilePrimitive.js
+++ Source/Scene/Vector3DTilePrimitive.js
@@ -641,14 +641,9 @@ import Vector3DTileBatch from './Vector3DTileBatch.js';
             }
         }
 
-        if (!needToRebatch) {
+        if (!needToRebatch || (defined(primitive._lastRebatchCount) && Math.abs(primitive._lastRebatchCount - length) < 10)) {
             primitive._batchDirty = false;
             return false;
-        }
-
-        if (needToRebatch && !primitive.forceRebatch && primitive._framesSinceLastRebatch < 120) {
-            ++primitive._framesSinceLastRebatch;
-            return;
         }
 
         batchedIndices.sort(compareColors);
@@ -658,6 +653,8 @@ import Vector3DTileBatch from './Vector3DTileBatch.js';
         } else {
             rebatchCPU(primitive, batchedIndices);
         }
+
+        primitive._lastRebatchCount = primitive._batchedIndices.length;
 
         primitive._framesSinceLastRebatch = 0;
         primitive._batchDirty = false;
