8a94f65949c8f6af70a7025e4225adc468e7ad77
aschilling <aschilling@host.docker.internal>
2023-07-03 13:43:25 +0200

    format

--- packages/engine/Source/Shaders/Builtin/Functions/acesTonemapping.glsl
+++ packages/engine/Source/Shaders/Builtin/Functions/acesTonemapping.glsl
@@ -2,20 +2,8 @@
 //    https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
 
 vec3 czm_acesTonemapping(vec3 color) {
-  /*  float g = 0.985;
-    float a = 0.065;
-    float b = 0.0001;
-    float c = 0.433;
-    float d = 0.238;
-
-    color = (color * (color + a) - b) / (color * (g * color + c) + d);
-
-    color = clamp(color, 0.0, 1.0);
-    */
-    
-    
-    /////////////////////////////////////////////////
-    // the formula above does not seem to be the one from ACES
+#ifdef USE_VCS_CUSTOM_SHADING
+    // the formula below does not seem to be the one from ACES
     float a = 2.51;
 	float b = 0.03;
 	float c = 2.43;
@@ -25,4 +13,17 @@ vec3 czm_acesTonemapping(vec3 color) {
     color = clamp(color, 0.0, 1.0);
 
     return color;
+#else
+    float g = 0.985;
+    float a = 0.065;
+    float b = 0.0001;
+    float c = 0.433;
+    float d = 0.238;
+
+    color = (color * (color + a) - b) / (color * (g * color + c) + d);
+
+    color = clamp(color, 0.0, 1.0);
+
+    return color;
+#endif
 }
