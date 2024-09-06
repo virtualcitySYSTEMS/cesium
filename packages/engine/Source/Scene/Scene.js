6e576526ad075f0d5332bfe37d0da41fce324999
Jannes Bolling <jbolling@virtualcitysystems.de>
2024-09-06 13:51:44 +0200

    Squashed commit of the following:
    commit 5048405d6c214fa51c8fae2e372fee5f1ba8724e
    Author: Ben Kuster <bkuster@vc.systems>
    Date:   Mon Aug 19 13:26:17 2024 +0200
    
        set frameNumber to 0 to trip update height callbacks
    
    commit dd0541d48e4072544d5bfbf8e55532e4a6710bfa
    Author: Ben Kuster <bkuster@vc.systems>
    Date:   Mon Aug 19 11:50:40 2024 +0200
    
        remove debuggin console.log
    
    commit d27b58ae8b9bbca33327e5fd56663499a0113447
    Author: Ben Kuster <bkuster@vc.systems>
    Date:   Mon Aug 19 11:49:15 2024 +0200
    
        changes.md
    
    commit 38fd997eec24d946f4936e563b17efe9fbca4eea
    Author: Ben Kuster <bkuster@vc.systems>
    Date:   Mon Aug 19 11:38:20 2024 +0200
    
        fix scene.updateHeight

--- packages/engine/Source/Scene/Scene.js
+++ packages/engine/Source/Scene/Scene.js
@@ -3746,7 +3746,17 @@ Scene.prototype.updateHeight = function (
   Check.typeOf.func("callback", callback);
   //>>includeEnd('debug');
 
+  let callbackWrapperCalledForFrameState = -1;
+  let removed = false;
+
   const callbackWrapper = () => {
+    if (
+      removed ||
+      this.frameState.frameNumber <= callbackWrapperCalledForFrameState
+    ) {
+      return;
+    }
+    callbackWrapperCalledForFrameState = this.frameState.frameNumber;
     Cartographic.clone(cartographic, updateHeightScratchCartographic);
 
     const height = this.getHeight(cartographic, heightReference);
@@ -3822,6 +3832,7 @@ Scene.prototype.updateHeight = function (
     tilesetRemoveCallbacks = {};
     removeAddedListener();
     removeRemovedListener();
+    removed = true;
   };
 
   return removeCallback;
